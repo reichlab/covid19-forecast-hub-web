0be82337c31a09f5a91ecdc7f7252f60a8ed7391
Abdul Hannan Kanji <akanji@umass.edu>
2020-06-11 16:02:33 -0400

    move git name setup

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,10 @@ bundle exec jekyll build -d docs
 # Go To Public folder
 # cd public
 
+if [ "$ENV" = "CI" ]; then
+	git config user.name "GitHub Action"
+	git config user.email "user@example.com"
+fi
 # Add changes to git.
 git add .
 
@@ -23,11 +27,6 @@ if [ -n "$*" ]; then
 	msg="$*"
 fi
 git commit -m "$msg"
-
-if [ "$ENV" = "CI" ]; then
-	git config user.name "GitHub Action"
-	git config user.email "user@example.com"
-fi
 
 # Push source and build repos.
 git push origin master 
