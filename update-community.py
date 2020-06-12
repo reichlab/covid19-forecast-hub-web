2eb9b71ec3512dd4d3405fa97c6b3ebb970da54e
Abdul Hannan Kanji <akanji@umass.edu>
2020-06-12 12:07:57 -0400

    rebuilding site Fri Jun 12 12:07:57 EDT 2020

--- update-community.py
+++ update-community.py
@@ -2,6 +2,7 @@ from zoltpy import util
 from zoltpy.connection import ZoltarConnection
 
 import yaml
+from datetime import date
 
 res_models = []
 file_name = '_data/community.yml'
@@ -10,14 +11,30 @@ conn = util.authenticate()
 project_name = 'COVID-19 Forecasts'
 project = None
 
-props = ['name', 'abbreviation', 'description', 'home_url']
+date_format = "%A, %d %B %Y"
+
+props = ['name', 'abbreviation', 'description', 'home_url', 'url']
 # models = util.print_models(conn, project_name = project_name)
 # print(project.models)
+
+
+def fetch_forecast_dates(con, forecasts):
+    dates = []
+    for f in forecasts:
+        dates.append(date.fromisoformat(con.json_for_uri(f)['time_zero']['timezero_date']))
+    return dates
+    pass
 
 def parse_model(model):
     result = {}
     for prop in props:
         result[prop] = model[prop]
+    result['forecasts'] = len(model['forecasts'])
+    dates = fetch_forecast_dates(conn, model['forecasts'])
+    result['latest_forecast'] = max(dates).strftime(date_format)
+    result['earliest_forecast'] = min(dates).strftime(date_format)
+    # print('latest:', max(dates).strftime(date_format))
+
     return result
     pass
 
@@ -27,12 +44,14 @@ def gen_community():
         if proj.name == project_name:
             project = proj
             break
+    # print(project.json)
     # get model json and parse it to a dictionary
     for m in project.models:
         model = m.json
-        # print(model)
+        print(model)
         # print(conn.json_for_uri(model['url']))
         res_models.append(parse_model(model))
+        break
     
     # write the data to /_data/community.yml
     with open(file_name, 'w') as file:
