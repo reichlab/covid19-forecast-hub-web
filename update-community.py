528e880b9adebda170cacc22cd21731bf67457c2
matthewcornell <matt@matthewcornell.org>
2022-09-07 13:06:37 -0400

    cleaned up and optimized update-community.py to use far fewer zoltar requests

--- update-community.py
+++ update-community.py
@@ -1,105 +1,90 @@
-from zoltpy import util
-from zoltpy.connection import ZoltarConnection
+from datetime import date
 
 import yaml
-from datetime import date, datetime, timedelta
+from zoltpy import util
 
-res_models = []
-team_d = {}
-file_name = '_data/community.yml'
-conn = util.authenticate()
 
-project_name = 'COVID-19 Forecasts'
-project = None
+FILE_NAME = '_data/community.yml'
+PROJECT_NAME = 'COVID-19 Forecasts'
+DATE_FORMAT = "%A, %d %B %Y"
+ZOLTAR_HOST_URL = 'https://zoltardata.com'
+DAYS_OLD = 14
 
-date_format = "%A, %d %B %Y"
-
-props = ['name', 'abbreviation', 'description', 'home_url', 'team_name']
-# models = util.print_models(conn, project_name = project_name)
-# print(project.models)
-base = 'https://zoltardata.com'
 
 def get_public_url(url):
     parts = url.strip('/').split('/')
     resource = parts[-2]
     res_id = parts[-1]
-    return '%s/%s/%s' % (base, resource, res_id)
+    return '%s/%s/%s' % (ZOLTAR_HOST_URL, resource, res_id)
 
-def fetch_forecast_dates(con, forecasts):
-    dates = []
-    for f in forecasts:
-        dates.append(date.fromisoformat(con.json_for_uri(f)['time_zero']['timezero_date']))
-    return dates
-    pass
 
 def parse_model(model):
-    result = {}
-    for prop in props:
-        result[prop] = model[prop]
-    result['forecasts'] = len(model['forecasts'])
+    """
+    Example return value:
+
+    {'name': 'DELPHI',
+     'abbreviation': 'CovidAnalytics-DELPHI',
+     'description': 'This model predicts based on an SEIR model augmented with underdetection and interventions. ... ',
+     'home_url': 'https://www.covidanalytics.io/',
+     'team_name': 'CovidAnalytics at MIT',
+     'forecasts': 116,
+     'latest_forecast': 'Monday, 04 July 2022',
+     'earliest_forecast': 'Wednesday, 22 April 2020',
+     'dates': [datetime.date(2021, 3, 29), datetime.date(2020, 4, 22), ..., datetime.date(2022, 5, 30)],
+     'url': 'https://zoltardata.com/model/196',
+     'actions': [{'label': 'Website', 'url': 'https://www.covidanalytics.io/', 'new_window': True},
+                 {'label': 'Forecast data', 'type': 'url', 'url': 'https://zoltardata.com/model/196', 'new_window': True}]}
+    """
+    result = {'name': model.json['name'], 'abbreviation': model.json['abbreviation'],
+              'description': model.json['description'], 'home_url': model.json['home_url'],
+              'team_name': model.json['team_name'], 'forecasts': len(model.json['forecasts'])}
+
     if result['forecasts'] == 0:
         return None
-    dates = fetch_forecast_dates(conn, model['forecasts'])
-    result['latest_forecast'] = max(dates).strftime(date_format)
-    result['earliest_forecast'] = min(dates).strftime(date_format)
-    result['dates'] = dates
-    result['url'] = get_public_url(model['url'])
-    result['actions'] = []
-    action = {}
-    action['label'] = 'Website'
-    # action['type'] = 'primary'
-    action['url'] = result['home_url']
-    action['new_window'] = True
-    # action['new_line'] = True
-    result['actions'].append(action)
-    result['actions'].append({'label':'Forecast data', 'type':'url', 'url': result['url'], 'new_window': True})
-    # print('latest:', max(dates).strftime(date_format))
 
+    dates = [forecast.timezero.timezero_date for forecast in model.forecasts]  # one query
+    result['latest_forecast'] = max(dates).strftime(DATE_FORMAT)
+    result['earliest_forecast'] = min(dates).strftime(DATE_FORMAT)
+    result['dates'] = dates
+    result['url'] = get_public_url(model.json['url'])
+    result['actions'] = [{'label': 'Website', 'url': result['home_url'], 'new_window': True},
+                         {'label': 'Forecast data', 'type': 'url', 'url': result['url'], 'new_window': True}]
     return result
-    pass
 
 
 def parse_teams(teams):
     res = []
     for team in teams:
-        # print(teams[team][0]['team_name'])
-        # print(list(map(lambda x: (date.today() - max(x['dates'])).days, teams[team])))
-        max_model = list(filter(lambda x: (date.today() - max(x['dates'])).days < 14 , teams[team]))
-        if(len(max_model) > 0):
-            max_model = max(max_model, key= lambda x: x['forecasts'])
+        max_model = list(filter(lambda _: (date.today() - max(_['dates'])).days < DAYS_OLD, teams[team]))
+        if max_model:
+            max_model = max(max_model, key=lambda _: _['forecasts'])
             res.append(max_model)
-        pass
     return res
 
+
 def gen_community():
-    # find project
-    for proj in conn.projects:
-        if proj.name == project_name:
-            project = proj
-            break
-    # print(project.json)
-    # get model json and parse it to a dictionary
-    for m in project.models:
-        model = m.json
-        # print(model)
-        # break
-        # print(conn.json_for_uri(model['url']))
-        m = parse_model(model)
-        # print(m)
-        if m is not None:
-            if m['team_name'] not in team_d:
-                team_d[m['team_name']] = []
-            team_d[m['team_name']].append(m)
-        # res_models.append()
-    # filter out teams that have no forecasts
-    teams_final = list(filter(lambda x: x is not None,parse_teams(team_d)))
-    res_models = sorted(parse_teams(team_d), key=lambda x: max(x['dates']), reverse=True)
-    
+    # get project
+    conn = util.authenticate()
+    projects = [project for project in conn.projects if project.name == PROJECT_NAME]
+    if len(projects) != 1:
+        raise RuntimeError(f"did not find the project. name={PROJECT_NAME!r}, projects={projects}")
+
+    project = projects[0]
+
+    # get model and convert it to a dictionary
+    team_d = {}
+    for model in project.models:
+        model = parse_model(model)
+        if model is not None:
+            if model['team_name'] not in team_d:
+                team_d[model['team_name']] = []
+            team_d[model['team_name']].append(model)
+
     # write the data to /_data/community.yml
-    with open(file_name, 'w') as file:
-        documents = yaml.dump(res_models, file)
-        print('Generated community file')
-    pass
+    res_models = sorted(parse_teams(team_d), key=lambda x: max(x['dates']), reverse=True)
+    with open(FILE_NAME, 'w') as file:
+        yaml.dump(res_models, file)
+
 
 if __name__ == '__main__':
     gen_community()
